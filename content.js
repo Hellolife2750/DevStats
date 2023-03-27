4ce9cc65ac068302822c2b8ed1ab0fa607c99ee8
Hellolife2750 <hellolife2750.programmation@gmail.com>
2023-03-27 19:38:53 +0200

    first commit

---
+++ content.js
@@ -0,0 +1,119 @@
+/* exécutée sur la page à l'ouverture du site */
+
+
+
+if (location.hostname === 'chat.openai.com') {
+    increment(location.hostname);
+    //alert("gpt");
+} else if (location.hostname === 'stackoverflow.com') {
+    increment(location.hostname);
+    //alert("stack");
+} else if (location.hostname === 'openclassrooms.com') {
+    increment('openclassrooms.com');
+    /*setTimeout(function () {
+        window.open(chrome.extension.getURL('popup.html'));
+    }, 1000); // Pause de 1 seconde*/
+
+
+}
+
+function increment(url) {
+    let oldValue = localStorage.getItem(url);
+    if (oldValue == null) {
+        localStorage.setItem(url, 1);
+    } else {
+        localStorage.setItem(url, parseInt(oldValue) + 1)
+    }
+
+    localStorage.setItem("grosconnard", "test")
+
+    chrome.storage.local.get(url, function (items) {
+        let oldValue = items[url];
+        if (oldValue == null) {
+            chrome.storage.local.set({ [url]: 1 });
+        } else {
+            chrome.storage.local.set({ [url]: parseInt(oldValue) + 1 });
+        }
+
+        /*chrome.storage.local.get(url, function (items) {
+            alert(items[url]);
+        });*/
+    });
+
+    // Créer un élément d'image
+    /*const logo = document.createElement('img');
+
+    // Définir les attributs de l'image
+    logo.src = 'icon.png';
+    logo.style.position = 'absolute';
+    logo.style.zIndex = '1000';
+    logo.style.top = '0';
+    logo.style.left = '0';
+    logo.style.width = '50px';
+    logo.style.height = '50px';
+
+    // Ajouter l'image à la page
+    document.body.appendChild(logo);*/
+
+
+    //alert(localStorage.getItem("grosconnard"))
+    //alert(localStorage.getItem(url))
+    //alert(localStorage.getItem('openclassrooms.com'))
+
+    /*const keysWithTestValue = [];
+
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const value = localStorage.getItem(key);
+
+        if (value === 'test') {
+            keysWithTestValue.push(key);
+        }
+    }
+
+    console.log(keysWithTestValue);*/
+}
+
+window.addEventListener("load", function () {
+    var img = document.createElement("img");
+    img.src = chrome.runtime.getURL("icon.png");
+    img.style.position = "absolute";
+    img.style.top = "0";
+    img.style.right = "0";
+    img.style.width = "50px";
+    img.style.height = "50px";
+    img.style.zIndex = "1000";
+
+    document.body.appendChild(img);
+});
+
+
+
+
+
+
+//content script
+/*var recetxt = ""
+chrome.extension.onRequest.addListener(
+    function (request, sender, sendResponse) {
+        recetxt = request.greeting
+    });
+alert(recetxt)*/
+
+
+
+//increment(location.hostname);
+
+/*// Stocker une valeur dans localStorage
+localStorage.setItem('maCle', 'maValeur');
+
+// Récupérer une valeur de localStorage
+const maValeur = localStorage.getItem('maCle');
+
+// Supprimer une valeur de localStorage
+localStorage.removeItem('maCle');
+
+// Vider tout le localStorage
+localStorage.clear();*/
+
+
---
+++ popup.js
@@ -0,0 +1,93 @@
+/* exécuté sur le popup*/
+
+const clearData = document.getElementById('clear-data');
+const visitedUl = document.getElementById("visited-ul");
+const lastClear = document.getElementById("last-clear");
+
+clearData.addEventListener('click', () => {
+    //localStorage.removeItem();
+    /*chrome.storage.local.remove("openclassrooms.com");
+    chrome.storage.local.remove("stackoverflow.com");
+    chrome.storage.local.remove("chat.openai.com");*/
+
+    chrome.storage.local.set({ ["openclassrooms.com"]: 0 });
+    chrome.storage.local.set({ ["stackoverflow.com"]: 0 });
+    chrome.storage.local.set({ ["chat.openai.com"]: 0 });
+
+    updateVisitedUl();
+    lastClear.querySelector(".date").innerText = getDate();
+    chrome.storage.local.set({ ["dev_stats_last_clear"]: getDate() });
+    //alert(localStorage.getItem('openclassrooms.com'))
+});
+
+function getDate() {
+    const date = new Date();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return day + '/' + month + '/' + year;
+}
+
+/*let counter = localStorage.getItem('https://openclassrooms.com/');
+if (counter == null) {
+    counter = 0;
+}
+document.getElementById('counter').textContent = counter;*/
+
+/*chrome.storage.local.get("openclassrooms.com", function (items) {
+    alert(items["openclassrooms.com"]);
+});*/
+
+function getValue(tkey, callback) {
+    chrome.storage.local.get(tkey, function (items) {
+        var value = items[tkey];
+        callback(value);
+    });
+}
+
+function updateVisitedUl() {
+    getValue("openclassrooms.com", function (value) {
+        visitedUl.querySelector(".openclassrroms .value").innerHTML = value;
+    });
+
+    getValue("stackoverflow.com", function (value) {
+        visitedUl.querySelector(".stackoverflow .value").innerHTML = value;
+    });
+
+    getValue("chat.openai.com", function (value) {
+        visitedUl.querySelector(".chatgpt .value").innerHTML = + value;
+    });
+}
+
+updateVisitedUl();
+
+getValue("dev_stats_last_clear", function (date) {
+    lastClear.querySelector(".date").innerText = date;
+});
+
+
+
+
+
+
+
+//popup
+/*var txt = "TEXT"
+chrome.extension.getBackgroundPage().chrome.tabs.getSelected(null,
+    function (tab) {
+        chrome.tabs.sendRequest(tab.id, { greeting: txt }, function (response) { });
+    });*/
+
+/*const keysWithTestValue = [];
+
+for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+
+    if (value === 'test') {
+        keysWithTestValue.push(key);
+    }
+}
+
+console.log(keysWithTestValue);
+alert(keysWithTestValue.length)*/
