6ab7e3a3393988a3c52d28d2397f8b009d7356e8
Hellolife2750 <hellolife2750.programmation@gmail.com>
2023-04-08 16:53:35 +0200

    rajout du tableau et de la possibilité de télécharger les données en csv

--- js/index.js
+++ js/index.js
@@ -141,6 +141,101 @@ function appearPopup(type, url) {
     }, 3500);
 }
 
+//fonction qui renvoie un tableau par ordre croissant des sites (clés) qui ont été le plus visités (stockés dans la cache)
+function getOrderedKeys() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(DAT_PATH, function (result) {
+            const data = result[DAT_PATH];
+            if (typeof data === "undefined" || data === null) return;
+            const keys = Object.keys(data);
+
+            const filteredKeys = keys.filter(key => !key.startsWith("Internal_"));
+
+            filteredKeys.sort((a, b) => {
+                const counterA = data[a].counter;
+                const counterB = data[b].counter;
+                return counterB - counterA;
+            });
+
+            resolve(filteredKeys);
+        });
+    });
+}
+
+var csvString = `Website,Visits,Last visit,Elapsed time`;
+
+function generateTable() {
+    getOrderedKeys().then(keys => {
+        chrome.storage.local.get(DAT_PATH, function (result) {
+            const data = result[DAT_PATH];
+            const tableBody = document.querySelector("#stats-table tbody");
+            tableBody.innerHTML = "";
+            for (let i = 0; i < keys.length; i++) {
+                const key = keys[i];
+                const urlData = data[key];
+                //const counter = urlData.counter;
+                //const valueSpan = visitedUl.children[i].querySelector('.value');
+                //valueSpan.textContent = counter;
+
+                const tableCode = `
+                <tr>
+                    <td>${key}</td>
+                    <td>${urlData.counter}</td>
+                    <td>${urlData.date}</td>
+                    <td>${urlData.elapsed}</td>
+                </tr>
+                `;
+
+                tableBody.insertAdjacentHTML('beforeend', tableCode);
+
+                csvString += `\n${key},${urlData.counter},${urlData.date},${urlData.elapsed}`;
+            }
+        });
+    });
+}
+
+generateTable();
+/*document.addEventListener('click', () => {
+    alert(csvString);
+})*/
+
+//convertir un chaine de caractères en tableau csv
+function csvToArray(csvString) {
+    let rows = csvString.split('\n');
+    let headers = rows[0].split(',');
+    let values = rows.slice(1).map(row => row.split(','));
+    return [headers, ...values];
+}
+
+//génère un fichier csv à partir d'un tableau
+function generateCsvFile(data) {
+    let csvContent = '';
+    data.forEach(row => {
+        let rowContent = row.join(',');
+        csvContent += rowContent + '\n';
+    });
+    let encodedUri = encodeURI(csvContent);
+    let link = document.createElement('a');
+    link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodedUri);
+    link.setAttribute('download', 'data.csv');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+//à appeler pour télécharger le fichier csv
+function downloadCsvFile(csvString) {
+    let data = csvToArray(csvString);
+    generateCsvFile(data);
+}
+
+const dlCsvBtn = document.getElementById('dl-csv-button');
+
+dlCsvBtn.addEventListener('click', () => {
+    downloadCsvFile(csvString);
+})
+
+
 
 
 
