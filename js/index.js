94861b80388ee96c0f40eb0a75d694440d51f318
Hellolife2750 <hellolife2750.programmation@gmail.com>
2023-04-21 16:41:24 +0200

    correction beug affichage last reset incorrect

--- js/index.js
+++ js/index.js
@@ -229,6 +229,7 @@ function generateTable() {
             setupSitesTooltips();
             createGraphics();
             setupRemoveSiteBtns();
+            updateLastReset();
 
             createTotalLine(keys.length, nbVisitsTotal, dateTotal, elapsedSecondsTotal);
         });
